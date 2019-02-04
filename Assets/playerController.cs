7ad961a65b6fc8ee0cbd5622ea90bc350f7a6cc1
Komich <turov.v@icloud.com>
2019-02-04 13:40:53 +0300

    1

---
+++ Assets/playerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public new Vector3 playerPos;
+    public float yPosition = 0f;
+    public float zPosition = 10f;
+    public float moveSpeed;
+    public Vector3 mousePos;
+
+    void Move()
+    {
+        
+    }
+
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Vector3 curPos = Vector3.Lerp(transform.position,new Vector3(mousePos.x, yPosition, zPosition), moveSpeed * Time.deltaTime);
+        //transform.position = curPos;
+
+        if (transform.position.x > 6)
+        {
+            transform.position = new Vector3(6, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < -6)
+        {
+            transform.position = new Vector3(-6, transform.position.y, transform.position.z);
+        }
+    }
+}
